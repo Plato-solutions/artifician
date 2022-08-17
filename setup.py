15dd8af74bf760a7f54d1842a0b9ef39aa2e1baf
abhijeet-plato <abhijeet.gandhi@platoanalytics.com>
2022-08-17 14:29:05 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="artifician",
+    version="0.1.4b1",
+    description="Artifician is an event driven framework developed to simplify the process of preparation of the dataset for Artificial Intelligence models.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    author="Abhijeet Gandhi",
+    author_email="abhijeet.gandhi@platoanalytics.com",
+    license="Apache-2.0",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+    ],
+    keywords="python artifician",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "pandas>=1.4.2",
+        "rx>=3.2.0",
+    ],
+    # project_urls={
+    #     'Documentation': "",
+    #     'Source': "",
+    #     'Tracker': "",
+    # },
+)
