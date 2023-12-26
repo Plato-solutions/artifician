22ec13f964f839a940db1f93baa57f36814c816d
abhijeet-plato <abhijeet.gandhi@platoanalytics.com>
2023-12-26 16:47:44 +0530

    update verion

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.rst").read_text()
+long_description = (this_directory / "README.md").read_text()
 with open("VERSION", "r") as f:
     version = f.read().strip()
 setup(
